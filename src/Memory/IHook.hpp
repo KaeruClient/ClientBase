6ea5408ac31cefd1bdb7d1f32497b2ae246b7af5
KaeruClient <KaeruClient@DESKTOP-VGCTKAV>
2026-05-24 20:22:01 +0900

    IHook修正

--- src/Memory/IHook.hpp
+++ src/Memory/IHook.hpp
@@ -12,24 +12,15 @@ class IHook {
 public:
     IHook() = default;
     virtual ~IHook() = default;
-
-    virtual void init() = 0;
-    virtual void uninit() {}
-
     void reset() { mHooks.clear(); }
-
 protected:
     template <auto detour, typename TReturn, typename... TArgs>
-    void createHook(const Address address, TReturn(*oFunc)(TArgs...)) {
-        if (!oFunc) {
-            throw std::invalid_argument("Original function pointer storage (oFunc) cannot be null.");
-        }
+    void createHook(const Address address, TReturn(*&oFunc)(TArgs...)) {
         // We're developing on x64, so we don't have to worry about calling conventions. Bro!
         auto managedDetour = +[](TArgs... args) -> TReturn {
             HookGuard guard;
-            return detour(std::forward<TArgs>(args)...);
+            return (*detour)(std::forward<TArgs>(args)...);
         };
-
         auto createResult = safetyhook::InlineHook::create(
             reinterpret_cast<void*>(address.mAddress),
             reinterpret_cast<void*>(managedDetour),
@@ -44,11 +35,10 @@ protected:
 
         auto hookPtr = std::make_unique<safetyhook::InlineHook>(std::move(createResult).value());
 
-        *oFunc = reinterpret_cast<TReturn(*)(TArgs...)>(hookPtr->trampoline().address());
+        oFunc = reinterpret_cast<TReturn(*)(TArgs...)>(hookPtr->trampoline().address());
 
         const auto enableResult = hookPtr->enable();
         if (!enableResult) {
-            *oFunc = nullptr;
             throw std::runtime_error(
                 "Failed to enable hook. Error Code: " + fmt::format("0x{:X}", static_cast<int>(enableResult.error().type))
             );
